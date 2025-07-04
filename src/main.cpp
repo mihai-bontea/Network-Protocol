0cb2fc17883bc0e2c9b01bebb83818ea071d626b
mihai-bontea <mihaibontea@protonmail.com>
2025-07-04 13:33:23 +0300

    folder re-organization to make gtest work

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "protocol.h"
+
+int main() {
+    run_protocol();
+    return 0;
+}
---
+++ src/protocol.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "protocol.h"
+
+void run_protocol() {
+    std::cout << "Running protocol logic!" << std::endl;
+}
