e58967f1b0250ea4caf137e24c26ffa3037ccfc4
mihai-bontea <mihaibontea@protonmail.com>
2025-08-01 22:57:30 +0300

    segment serialize full test

--- test/TcpSegmentTests.cpp
+++ test/TcpSegmentTests.cpp
@@ -2,6 +2,56 @@
 
 #include "TCPSegment.h"
 
+TEST(TcpSegmentTest, SerializeSegment)
+{
+    TCPHeader header;
+    header.source_port = 0x1234;
+    header.destination_port = 0xABCD;
+    header.sequence_number = 0x11223344;
+    header.acknowledgment_number = 0x55667788;
+    header.data_offset = 5;
+    header.reserved = 0;
+    header.flags = 0x18;
+    header.window_size = 0x4000;
+    header.checksum = 0xBEEF;
+    header.urgent_pointer = 0x0000;
+
+    TCPSegment segment;
+    segment.header = header;
+    segment.payload = {'T', 'E', 'S', 'T'};
+
+    const size_t segment_size = 24;
+    ASSERT_EQ(segment.size(), segment_size);
+
+    const auto buffer = segment.serialize();
+
+    EXPECT_EQ(buffer[0], 0x12);
+    EXPECT_EQ(buffer[1], 0x34);
+    EXPECT_EQ(buffer[2], 0xAB);
+    EXPECT_EQ(buffer[3], 0xCD);
+    EXPECT_EQ(buffer[4], 0x11);
+    EXPECT_EQ(buffer[5], 0x22);
+    EXPECT_EQ(buffer[6], 0x33);
+    EXPECT_EQ(buffer[7], 0x44);
+    EXPECT_EQ(buffer[8], 0x55);
+    EXPECT_EQ(buffer[9], 0x66);
+    EXPECT_EQ(buffer[10], 0x77);
+    EXPECT_EQ(buffer[11], 0x88);
+    EXPECT_EQ(buffer[12], 0x50);
+    EXPECT_EQ(buffer[13], 0x18);
+    EXPECT_EQ(buffer[14], 0x40);
+    EXPECT_EQ(buffer[15], 0x00);
+    EXPECT_EQ(buffer[16], 0xBE);
+    EXPECT_EQ(buffer[17], 0xEF);
+    EXPECT_EQ(buffer[18], 0x00);
+    EXPECT_EQ(buffer[19], 0x00);
+
+    EXPECT_EQ(buffer[20], 'T');
+    EXPECT_EQ(buffer[21], 'E');
+    EXPECT_EQ(buffer[22], 'S');
+    EXPECT_EQ(buffer[23], 'T');
+}
+
 TEST(TcpSegmentTest, DeserializeValidSegment)
 {
     uint8_t buffer[] = {
